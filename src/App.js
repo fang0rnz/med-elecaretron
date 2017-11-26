5519f62ad6f4e11d4a5e607849bde9878896403b
unknown <lucas.sartm@gmail.com>
2017-11-26 20:09:44 -0200

    added bootstrap and started styling component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="aligner">
+        <div className="aligner-item">
+          <h4>Manage your hospital:</h4>
+          <form className="form-inline">
+            <div className="form-group">
+              <input type="text" required placeholder="Hospital name" className="form-control" />
+              <button action="submit" className="btn btn-primary">
+                Go ->
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
