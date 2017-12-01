10bd3758669e80e09ec0bc5876ef6d2a5d4b3483
fangorn <lucas.sartm@gmail.com>
2017-12-01 17:14:34 -0200

    including bootstrap css

--- src/components/PatientForm.js
+++ src/components/PatientForm.js
@@ -20,6 +20,7 @@ class PatientForm extends Component {
       },
       isCreating: true,
     };
+    this.submit = this.submit.bind(this);
   }
 
   componentWillMount() {
@@ -43,7 +44,8 @@ class PatientForm extends Component {
       });
   }
 
-  submit() {
+  submit(e) {
+    e.preventDefault();
     if (!this.state.isCreating) {
       this.props.edit(this.state.patient);
       this.props.toggleEdit();
@@ -126,98 +128,114 @@ class PatientForm extends Component {
     return (
       <div className="card">
         <div className="card-body">
-          <div className="input-group">
-            <input
-              id="name"
-              value={this.state.patient.name}
-              onChange={this.changeFullName.bind(this)}
-              type="text"
-              className="form-control"
-              name="name"
-              placeholder="Full name"
-            />
-
-            <input
-              id="email"
-              value={this.state.patient.email}
-              onChange={this.changeEmail.bind(this)}
-              type="email"
-              className="form-control"
-              name="email"
-              placeholder="E-mail"
-            />
-          </div>
-
-          <div className="input-group">
-            <MaskedInput
-              mask="11/11/1111"
-              value={this.state.patient.birthdate}
-              onChange={this.changeBirthdate.bind(this)}
-              className="form-control"
-              name="birthdate"
-              placeholder="Birthdate"
-            />
-            <MaskedInput
-              mask="(11) 11111-1111"
-              value={this.state.patient.number}
-              onChange={this.changeCellphone.bind(this)}
-              className="form-control"
-              name="number"
-              placeholder="Cellphone number"
-            />
-          </div>
-          <div className="input-group">
-            <input
-              id="street"
-              value={this.state.patient.address.street}
-              onChange={this.changeStreetName.bind(this)}
-              type="text"
-              className="form-control"
-              name="street"
-              placeholder="Street"
-            />
-            <input
-              value={this.state.patient.address.number}
-              onChange={this.changeAddrNum.bind(this)}
-              type="text"
-              className="form-control"
-              name="address-number"
-              placeholder="Addr. number"
-              maxLength={5}
-            />
-
-            <MaskedInput
-              mask="11111-111"
-              value={this.state.patient.address.zip}
-              onChange={this.changeZipCode.bind(this)}
-              type="text"
-              className="form-control"
-              name="zipcode"
-              placeholder="Zip code"
-            />
-            <MaskedInput
-              mask="AA"
-              value={this.state.patient.address.state}
-              onChange={this.changeState.bind(this)}
-              type="text"
-              className="form-control"
-              name="address-state"
-              placeholder="State"
-            />
-          </div>
+          <form onSubmit={this.submit}>
+            <div className="row">
+              <div className="col-12">
+                <div className="input-group">
+                  <input
+                    id="name"
+                    value={this.state.patient.name}
+                    onChange={this.changeFullName.bind(this)}
+                    type="text"
+                    className="form-control"
+                    name="name"
+                    required="true"
+                    placeholder="Full name"
+                  />
+                  <input
+                    id="email"
+                    value={this.state.patient.email}
+                    onChange={this.changeEmail.bind(this)}
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    required="true"
+                    placeholder="E-mail"
+                  />
+                </div>
+                <div className="input-group">
+                  <input
+                    placeholder="DD/MM/YYYY - Birthdate"
+                    value={this.state.patient.birthdate}
+                    onChange={this.changeBirthdate.bind(this)}
+                    className="form-control"
+                    required="true"
+                    pattern={'[012][0-9]/[012][0-9]/[0-9]{4}'}
+                    maxLength={10}
+                    minLength={10}
+                    name="birthdate"
+                  />
+                  <MaskedInput
+                    mask="(11) 11111-1111"
+                    value={this.state.patient.number}
+                    onChange={this.changeCellphone.bind(this)}
+                    className="form-control"
+                    name="number"
+                    required="true"
+                    placeholder="Cellphone number"
+                  />
+                </div>
+                <div className="input-group">
+                  <input
+                    id="street"
+                    value={this.state.patient.address.street}
+                    onChange={this.changeStreetName.bind(this)}
+                    type="text"
+                    required="true"
+                    className="form-control"
+                    name="street"
+                    placeholder="Street"
+                  />
+                  <input
+                    value={this.state.patient.address.number}
+                    onChange={this.changeAddrNum.bind(this)}
+                    type="text"
+                    className="form-control"
+                    required="true"
+                    name="address-number"
+                    placeholder="Addr. number"
+                    maxLength={5}
+                  />
+                  <MaskedInput
+                    mask="11111-111"
+                    value={this.state.patient.address.zip}
+                    onChange={this.changeZipCode.bind(this)}
+                    type="text"
+                    required="true"
+                    className="form-control"
+                    name="zipcode"
+                    placeholder="Zip code"
+                  />
+                  <MaskedInput
+                    mask="AA"
+                    value={this.state.patient.address.state}
+                    onChange={this.changeState.bind(this)}
+                    type="text"
+                    required="true"
+                    className="form-control"
+                    name="address-state"
+                    placeholder="State"
+                  />
+                </div>
+              </div>
+              <div className="col-12">
+                <div className="row mt-3">
+                  <div className="col-6">
+                    <button type="submit" className="btn btn-success btn-block">
+                      Submit
+                    </button>
+                  </div>
+                  <div className="col-6">
+                    <button onClick={this.props.toggleEdit} type="button" className="btn btn-danger btn-block">
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
         </div>
-        <div className="row mb-3">
-          <div className="col-6">
-            <button onClick={this.submit.bind(this)} type="button" className="btn btn-success btn-block">
-              Submit
-            </button>
-          </div>
-          <div className="col-6">
-            <button onClick={this.props.toggleEdit} type="button" className="btn btn-danger btn-block">
-              Cancel
-            </button>
-          </div>
-        </div>
+        <div className="row mb-3" />
       </div>
     );
   }
